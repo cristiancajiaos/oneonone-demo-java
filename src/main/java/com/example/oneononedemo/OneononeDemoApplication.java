f91494a83bdb8defb6a8fbfde8fed3dd3f2783f2
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-02 09:36:53 -0400

    Primer commit

---
+++ src/main/java/com/example/oneononedemo/OneononeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oneononedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneononeDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OneononeDemoApplication.class, args);
+  }
+
+}
